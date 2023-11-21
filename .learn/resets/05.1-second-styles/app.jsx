a8bbd731c534112d5f2ca1d660d0ab6920ca990f
ingdev8023 <144868641+ingdev8023@users.noreply.github.com>
2023-11-21 20:37:28 +0000

    React Practice completed

--- .learn/resets/05.1-second-styles/app.jsx
+++ .learn/resets/05.1-second-styles/app.jsx
@@ -4,15 +4,11 @@ import PropTypes from "prop-types";
 
 const buttonStyles = {
 	//write button styles here
-	"background": "yellow",
-    "border": "none",
-    "color": "black",
+
 };
 
 const badgeStyles = {
 	//write the span styles here
-	"background": "red",
-    "borderRadius": "50%",
 
 };
 
